34b56b2b2457bbb30005922a2f5c7b42023aceb8
Bilous.liza@bk.ru <Bilous.liza@bk.ru>
2025-04-17 19:14:49 +0500

    Task1

---
+++ Tyuiu.KardonKD.Sprint4.Task1.V27.Test/DataServiceTest.cs
@@ -0,0 +1,17 @@
+using Tyuiu.KardonKD.Sprint4.Task1.V27.Lib;
+namespace Tyuiu.KardonKD.Sprint4.Task1.V27.Test
+{
+    [TestClass]
+    public class DataServiceTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            DataService ds = new DataService();
+            int wait = 256;
+            int[] array = new int[] { 8, 1, 4, 4, 1, 2, 3, 5, 1, 5 };
+            int res = ds.Calculate(array);
+            Assert.AreEqual(wait, res);
+        }
+    }
+}
---
+++ Tyuiu.KardonKD.Sprint4.Task1.V27/Program.cs
@@ -0,0 +1,35 @@
+﻿using Tyuiu.KardonKD.Sprint4.Task1.V27.Lib;
+namespace Tyuiu.KardonKD.Sprint4.Task1.V27
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            DataService ds = new DataService();
+
+            Console.Title = "Спринт #4 | Выполнила: Кордон К.Д. | ИСТНб-24-1";
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* Спринт #4                                                               *");
+            Console.WriteLine("* Тема: Оператор цикла foreach                                            *");
+            Console.WriteLine("* Задание #1                                                              *");
+            Console.WriteLine("* Вариант #27                                                             *");
+            Console.WriteLine("* Выполнила: Кордон Ксения Дмитриевна | ИСТНб-24-1                        *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+            Console.WriteLine("***************************************************************************");
+
+            int[] array = new int[] { 8, 1, 4, 4, 1, 2, 3, 5, 1, 5 };
+
+            Console.WriteLine("Массив = 8, 1, 4, 4, 1, 2, 3, 5, 1, 5");
+
+            int s = ds.Calculate(array);
+
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+            Console.WriteLine("***************************************************************************");
+
+            Console.WriteLine(s);
+            Console.ReadKey();
+        }
+    }
+}
