d75cdddada2cdd558d7d8b7345fe641cec10b737
Bilous.liza@bk.ru <Bilous.liza@bk.ru>
2025-04-17 20:16:57 +0500

    2

---
+++ Tyuiu.KardonKD.Sprint4.Task2.V3.Test/DaraServiceTest.cs
@@ -0,0 +1,17 @@
+using Tyuiu.KardonKD.Sprint4.Task2.V3.Lib;
+namespace Tyuiu.KardonKD.Sprint4.Task2.V3.Test
+{
+    [TestClass]
+    public class DaraServiceTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            DataService ds = new DataService();
+            int[] numsArray = { 2, 3, 4, 5, 6, 7, 2, 3, 4, 5, 6, 7 };
+            int res = ds.Calculate(numsArray);
+            int wait = 24;
+            Assert.AreEqual(res, wait);
+        }
+    }
+}
