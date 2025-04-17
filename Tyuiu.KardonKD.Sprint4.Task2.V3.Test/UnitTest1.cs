a6dcdd1d2e456eeac051e10978b48c45e1351ebf
Bilous.liza@bk.ru <Bilous.liza@bk.ru>
2025-04-17 20:13:55 +0500

    2

---
+++ Tyuiu.KardonKD.Sprint4.Task2.V3.Test/UnitTest1.cs
@@ -0,0 +1,11 @@
+namespace Tyuiu.KardonKD.Sprint4.Task2.V3.Test
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
---
+++ Tyuiu.KardonKD.Sprint4.Task2.V3/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Tyuiu.KardonKD.Sprint4.Task2.V3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
