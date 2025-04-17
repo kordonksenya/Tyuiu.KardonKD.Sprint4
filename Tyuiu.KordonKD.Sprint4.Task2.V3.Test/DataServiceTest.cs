14b6287a717d9e572cbee454dce64a1d513aa8a7
Bilous.liza@bk.ru <Bilous.liza@bk.ru>
2025-04-17 20:03:16 +0500

    Task2

---
+++ Tyuiu.KordonKD.Sprint4.Task2.V3.Test/DataServiceTest.cs
@@ -0,0 +1,17 @@
+using Tyuiu.KordonKD.Sprint4.Task2.V3.Lib;
+namespace Tyuiu.KordonKD.Sprint4.Task2.V3.Test
+{
+    [TestClass]
+    public class DataServiceTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            DataService ds = new DataService();
+            int[] numsArray = { 2, 3, 4, 5, 6, 7, 2, 3, 4, 5, 6, 7 };
+            int res = ds.Calculate(numsArray);
+            int wait = 24;
+            Assert.AreEqual(res, wait);
+        }
+    }
+}
